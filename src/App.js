ea6cb9a1fb8ecfcf928fdd112511411f76dd9d0c
Zsigmond Péter <zspeti02@gmail.com>
2022-12-30 00:12:55 +0100

    Add menu and hero into webpage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-//import logo from './logo.svg';
-//import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="sidebar">
+        <p className="title">peterzsigmond.dev</p>
+
+        <div className="menu">
+          <div className="element">
+            <img src="" />
+            <p>About</p>
+          </div>
+          <div className="element">
+            <img src="" />
+            <p>Projects</p>
+          </div>
+          <div className="element">
+            <img src="" />
+            <p>Contact</p>
+          </div>
+          <div className="element">
+            <img src="" />
+            <p>Cheatsheet</p>
+          </div>
+        </div>
+
+      </div>
+
+      <div className="main">
+        <div className="hero">
+          <div className="left">
+            <h1>Hi there, I'm Peter Zsigmond!</h1>
+            <h2>IT Engineer</h2>
+            <p>I'm into web development, software development and DevOps. I graduated from university in 2023.</p>
+            <button>Check out my Projects</button>
+          </div>
+          <div className="right">
+            <img src="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
