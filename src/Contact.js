08563f235718d8e08e3a0e8a8195e4537d43824c
Zsigmond Péter <zspeti02@gmail.com>
2023-02-16 11:47:32 +0100

    Add Contact page content

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,11 @@
+import './Contact.scss';
+
 function Contact() {
     return (
-          <div><p>contact</p></div>
+          <div className="contact">
+            <h2>Peter Zsigmond</h2>
+            <p>Email: <a href="mailto:zspeti02@gmail.com">zspeti02@gmail.com</a></p>
+          </div>
     );
   }
   
