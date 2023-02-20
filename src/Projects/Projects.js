875c3de878c29249e7679528b1cc46db62d2c2d0
Zsigmond Péter <zspeti02@gmail.com>
2023-02-20 17:17:43 +0100

    Update Projects page content and style

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -4,6 +4,11 @@ function Projects() {
 
     let projects = [
             {
+                  img: 'kubeflow',
+                  title: 'Bachelor\'s thesis',
+                  desc: 'Title: Management of machine learning application on a distributed container platform\n\nI gained a lot of experience, while making my bachelor\'s thesis. \n\nLink: https://bit.ly/3XIB6pl'
+            },
+            {
                   img: 'peterzsigmondOS',
                   title: 'peterzsigmondOS',
                   desc: 'A very basic x86 OS. It includes some images, and a terminal, where the users can write their own hex coded x86 instructions, which then the OS can run. Theoretically, with this terminal, any task can be done on a computer. Link: https://github.com/PeterZsigmond/peterzsigmondOS'
@@ -12,11 +17,6 @@ function Projects() {
                   img: 'peterzsigmond.dev',
                   title: 'peterzsigmond.dev',
                   desc: 'This personal portfolio webpage. It was made in React.'
-            },
-            {
-                  img: 'peterzsigmond-thesis',
-                  title: 'Bachelor\'s thesis',
-                  desc: 'My bachelor\'s thesis, titled: Management of machine learning application on a distributed container platform. Link: https://bit.ly/3XIB6pl'
             }
     ];
 
@@ -28,7 +28,9 @@ function Projects() {
                   {projects.map((project, index) => {
                         return (
                               <div className="card" key={index}>
-                                    <img src={require("../img/projects/" + project.img + ".png")} alt={project.title} />
+                                    <div className="image">
+                                          <img src={require("../img/projects/" + project.img + ".png")} alt={project.title} />
+                                    </div>
                                     <h3>{project.title}</h3>
                                     <p>{project.desc}</p>
                               </div>
