a1e78a9d0cf25425c8670b92a5a53849d498cf3f
Zsigmond Péter <zspeti02@gmail.com>
2023-03-04 01:06:15 +0100

    Make Contact page responsive

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,9 +2,12 @@ import './Contact.scss';
 
 function Contact() {
     return (
-          <div className="contact">
-            <h2>Peter Zsigmond</h2>
-            <p>Email: <a href="mailto:zspeti02@gmail.com">zspeti02@gmail.com</a></p>
+          <div className="container">
+            <h1>Contact</h1>
+            <div className="contact">
+              <h2>Peter Zsigmond</h2>
+              <p>Email: <a href="mailto:zspeti02@gmail.com">zspeti02@gmail.com</a></p>
+            </div>
           </div>
     );
   }
