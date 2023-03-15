c600302a6ba6c822356c0b7ab9c81123552f4b07
Zsigmond Péter <zspeti02@gmail.com>
2023-03-15 22:31:14 +0100

    Add Cheatsheet page structure

--- src/Components/Cheatsheet/Cheatsheet.js
+++ src/Components/Cheatsheet/Cheatsheet.js
@@ -1,13 +1,53 @@
+import { useState } from 'react';
+import parse from 'html-react-parser';
+import { cheatsheets } from "./CheatSheets";
 import './Cheatsheet.scss';
 
 function Cheatsheet() {
+
+	const [selected, setSelected] = useState([0, 0]);
+	let selectedTopic = cheatsheets[selected[0]].topics[selected[1]];
+
     return (
-          <div className="cheatsheet">
-            <h1>Cheatsheet</h1>
-            <div className="come-soon">
-                  <p>This page will come soon.</p>
-            </div>
-          </div>
+		<div className="cheatsheet">
+			<h1>Cheatsheet</h1>
+
+			<div className="content">
+
+				<div className="categories">
+					{cheatsheets.map((category, index_0) => {
+						return (
+							<div className="category" key={index_0}>
+								<div className="name">
+									<img src={require("../../Images/cheatsheets/" + category.img + ".png")} alt={category.category} />
+									<p>{category.category}</p>
+								</div>
+								{category.topics.map((topic, index_1) => {
+									return (
+										<p className="topic" key={index_1} onClick={() => setSelected([index_0, index_1])}>{topic.name}</p>
+									);
+								})}
+							</div>
+						);
+					})}
+				</div>
+
+				<div className="selected-topic">
+					<h3 className="name">{selectedTopic.name}</h3>
+					{selectedTopic.cheats.map((cheat, index) => {
+						return (
+							<div key={index}>
+								<h4 className="title">{cheat.name}</h4>
+								<p className="code">{parse(cheat.code)}</p>
+								<p className="desc">{parse(cheat.desc)}</p>
+							</div>
+						);
+					})}
+				</div>
+
+			</div>
+
+		</div>
     );
   }
   
