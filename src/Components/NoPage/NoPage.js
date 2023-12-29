5812ed7b5405c65741c42b825859e6f49a03e96e
Peter Zsigmond <zspeti02@gmail.com>
2023-12-29 23:47:31 +0100

    Fix indentation in NoPage component

--- src/Components/NoPage/NoPage.js
+++ src/Components/NoPage/NoPage.js
@@ -1,12 +1,11 @@
 import './NoPage.scss';
 
 function NoPage() {
-    return (
-          <div className="nopage">
-            <h3>This page is not found!</h3>
-          </div>
-    );
-  }
-  
-  export default NoPage;
-  
+	return (
+		<div className="nopage">
+			<h3>This page is not found!</h3>
+		</div>
+	);
+}
+
+export default NoPage;
