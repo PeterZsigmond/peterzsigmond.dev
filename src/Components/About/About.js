ca17c62ff5daf29a8fa29e42d60900ee5e0ebf09
Peter Zsigmond <zspeti02@gmail.com>
2023-12-29 23:23:52 +0100

    Fix indentation in About component

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,79 +4,78 @@ import './About.scss';
 
 function About() {
 
-  let skills = [
-    [
-      {name: 'VS Code', img: 'vs-code'},
-      {name: 'Git', img: 'git'},
-      {name: 'Photoshop', img: 'photoshop'},
-      {name: 'Figma', img: 'figma'},
-      {name: 'Docker', img: 'docker'},
-      {name: 'Kubernetes', img: 'kubernetes'},
-      {name: 'AWS', img: 'aws'}
-    ],
-    [
-      {name: 'Assembly', img: 'asm'},
-      {name: 'C', img: 'c'},
-      {name: 'C++', img: 'cpp'},
-      {name: 'C#', img: 'c-sharp'},
-      {name: 'Java', img: 'java'},
-      {name: 'PHP', img: 'php'},
-      {name: 'JavaScript', img: 'js'},
-      {name: 'Python', img: 'python'},
-      {name: 'SQL*', img: 'sql'}
-    ]
-  ];
-  
-  const [skill, setSkill] = useState(0);
+	let skills = [
+		[
+			{name: 'VS Code', img: 'vs-code'},
+			{name: 'Git', img: 'git'},
+			{name: 'Photoshop', img: 'photoshop'},
+			{name: 'Figma', img: 'figma'},
+			{name: 'Docker', img: 'docker'},
+			{name: 'Kubernetes', img: 'kubernetes'},
+			{name: 'AWS', img: 'aws'}
+		],
+		[
+			{name: 'Assembly', img: 'asm'},
+			{name: 'C', img: 'c'},
+			{name: 'C++', img: 'cpp'},
+			{name: 'C#', img: 'c-sharp'},
+			{name: 'Java', img: 'java'},
+			{name: 'PHP', img: 'php'},
+			{name: 'JavaScript', img: 'js'},
+			{name: 'Python', img: 'python'},
+			{name: 'SQL*', img: 'sql'}
+		]
+	];
 
-  let elements = skills[skill];
+	const [skill, setSkill] = useState(0);
 
-  return (
-      <div>
-        <div className="hero">
-          <div className="left">
-            <h1>Hi there,<br />I'm <span>Peter Zsigmond!</span></h1>
-            <h2>IT Engineer</h2>
-            <p>I'm into Web development and DevOps.</p>
-            <Link to="/projects"><button>Check out my Projects</button></Link>
-          </div>
-          <div className="right">
-            <div className="circle">
-              <img src={require("../../Images/peterzsigmond/peterzsigmond.png")} alt="Peter Zsigmond" />
-            </div>
-          </div>
-        </div>
+	let elements = skills[skill];
 
-        <div className="skills">
-          <div className="selector">
-            <div className="left">
-              <div className={"box" + (skill === 0 ? ' active': '')} onClick={() => setSkill(0)}>
-                <p>Tools I Use for Development</p>
-              </div>
-            </div>
-            <div className="right">
-              <div className={"box" + (skill === 1 ? ' active': '')} onClick={() => setSkill(1)}>
-                <p>Programming Languages I Know</p>
-              </div>
-            </div>
-          </div>
+	return (
+		<div>
+			<div className="hero">
+				<div className="left">
+					<h1>Hi there,<br />I'm <span>Peter Zsigmond!</span></h1>
+					<h2>IT Engineer</h2>
+					<p>I'm into Web development and DevOps.</p>
+					<Link to="/projects"><button>Check out my Projects</button></Link>
+				</div>
+				<div className="right">
+					<div className="circle">
+						<img src={require("../../Images/peterzsigmond/peterzsigmond.png")} alt="Peter Zsigmond" />
+					</div>
+				</div>
+			</div>
 
-          <div className="elements">
+			<div className="skills">
 
-          {elements.map((element, index) => {
-              return (
-                <div className="element" key={index}>
-                  <img src={require("../../Images/skills/" + element.img + ".png")} alt={element.name} />
-                  <p>{element.name}</p>
-                </div>
-              );
-          })}
+				<div className="selector">
+					<div className="left">
+						<div className={"box" + (skill === 0 ? ' active': '')} onClick={() => setSkill(0)}>
+							<p>Tools I Use for Development</p>
+						</div>
+					</div>
+					<div className="right">
+						<div className={"box" + (skill === 1 ? ' active': '')} onClick={() => setSkill(1)}>
+							<p>Programming Languages I Know</p>
+						</div>
+					</div>
+				</div>
 
-          </div>
+				<div className="elements">
+					{elements.map((element, index) => {
+						return (
+							<div className="element" key={index}>
+								<img src={require("../../Images/skills/" + element.img + ".png")} alt={element.name} />
+								<p>{element.name}</p>
+							</div>
+						);
+					})}
+				</div>
 
-        </div>
-      </div>
-  );
+			</div>
+		</div>
+	);
 }
 
 export default About;
