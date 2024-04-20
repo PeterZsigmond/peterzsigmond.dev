7e7624e2bd1cd28bf032099dff0e1b7054af8b7e
Peter Zsigmond <zspeti02@gmail.com>
2024-04-20 20:27:52 +0200

    Add date for projects in Projects page

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -6,14 +6,16 @@ function Projects() {
 		{
 			img: 'salesforce-certification',
 			title: 'Salesforce Certification',
-			desc: 'I got Salesforce certified as a Platform Developer I.'
+			desc: 'I got Salesforce certified as a Platform Developer I.',
+			date: 'August, 2023'
 		},
 		{
 			img: 'kubeflow',
 			title: 'Bachelor\'s thesis',
 			desc: 'Title: Management of machine learning application on a distributed container platform\n\n' +
 				'While making my bachelor\'s thesis, I gained experiences with Machine Learning, Container orchestration, Automatization, Cloud services, etc. ' +
-				'I used Kubeflow as a ML workflow toolset, that runs on Kubernetes.'
+				'I used Kubeflow as a ML workflow toolset, that runs on Kubernetes.',
+			date: 'December, 2022'
 		},
 		{
 			img: 'peterzsigmondOS',
@@ -22,6 +24,7 @@ function Projects() {
 				'where the users can write their own hex coded x86 instructions, which the OS can then run. ' +
 				'Theoretically, with this terminal, any task can be done on a computer.\n\n' +
 				'I learned a lot about how the x86 architecture works.',
+			date: 'February, 2023',
 			link_text: 'GitHub',
 			link: 'https://github.com/PeterZsigmond/peterzsigmondOS'
 		},
@@ -29,6 +32,7 @@ function Projects() {
 			img: 'peterzsigmond.dev',
 			title: 'peterzsigmond.dev',
 			desc: 'This is my personal portfolio website. I made it in React, and used other tools like Sass and Docker.',
+			date: 'March, 2023',
 			link_text: 'GitHub',
 			link: 'https://github.com/PeterZsigmond/peterzsigmond.dev'
 		}
@@ -46,7 +50,8 @@ function Projects() {
 								<img src={require("../../Images/projects/" + project.img + ".png")} alt={project.title} />
 							</div>
 							<h3>{project.title}</h3>
-							<p>{project.desc}</p>
+							<p className="desc">{project.desc}</p>
+							<p className="date">{project.date}</p>
 							{!project.link ||
 								<a href={project.link} target="_blank" rel="noreferrer">{project.link_text} <i className="icon bi-box-arrow-up-right"></i></a>
 							}
